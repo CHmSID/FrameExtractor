b2efd2e9b555eddc300970526e23c49620064979
Jerzy Baran <1chmsid@gmail.com>
2015-12-19 15:26:22 +0000

    Start moving codebase to Python

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FrameExtractor
 A utility application for extracting frames from videos
+
+##How to prepare
+* install opencv* packages
+* install python-opencv
+* install python
---
+++ extract.py
@@ -0,0 +1,18 @@
+# Written by Jerzy Baran 2015
+
+# This code takes filenames (videos) as arguments and extracts each frame
+# from them.
+
+import cv2
+
+# This is a test function
+def someFunction(num):
+	"""Prints the parameter"""
+
+	print num
+
+def main():
+	print cv2.__version__
+
+if __name__ == "__main__":
+	main()
